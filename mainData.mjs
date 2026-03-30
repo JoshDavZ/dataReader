b6b43306c1b2541dcc8678de3a18eef6a1c3ca0f
JoshDavZ <145669955+JoshDavZ@users.noreply.github.com>
2026-03-30 16:57:01 +0700

    Update and rename main.js to mainData.mjs

---
+++ mainData.mjs
@@ -0,0 +1,24 @@
+import { reader } from './read-xlsx.mjs';
+import { renderData } from './dataRender.mjs';
+import { filterByColumn, filterByMultipleColumns} from './dataFilters.js';
+import { SortByNumber,reduceBycolumn } from './Sort.js';
+
+reader.onReady((data) => {
+  const DegradasiFO = filterByColumn(reader.data, 'rca', 'DEGRADASI FO', 'include');
+  // const rcaData = reduceData(reader.data, ['rca']);
+  renderData(DegradasiFO, 'output2');
+  
+  
+  reader.data.forEach((row) => {
+    if (row['detail_rca']!==null && row['detail_rca']!=='') {
+    row['count'] = filterByColumn(data, 'rca', row['detail_rca'], 'include').length;
+    console.log('count for', row['detail_rca'], ':', row['count']);
+    }
+  });
+  let countData = reduceBycolumn(data, ['detail_rca','count']);
+  console.log('before sorting:', countData);
+  countData = SortByNumber(countData, 'count');
+  console.log('after sorting:', countData);
+  renderData(countData, 'output1');
+});
+
